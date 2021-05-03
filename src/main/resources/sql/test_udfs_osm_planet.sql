b7447ce6ed2a6efb6131631775a0cded54266cf8
Daniel Cortez <daniel@jyde.io>
2021-05-03 02:38:01 +0200

    Handle null input and DistanceUndefinedException for H3 Distance functions

--- src/main/resources/sql/test_udfs_osm_planet.sql
+++ src/main/resources/sql/test_udfs_osm_planet.sql
@@ -1,9 +1,6 @@
 USING EXTERNAL FUNCTION geotoh3(lat DOUBLE, lng DOUBLE, res INTEGER)
 RETURNS BIGINT
 LAMBDA 'h3-athena-udf-handler',
-EXTERNAL FUNCTION geotoh3address(lat DOUBLE, lng DOUBLE, res INTEGER)
-RETURNS VARCHAR
-LAMBDA 'h3-athena-udf-handler',
 EXTERNAL FUNCTION h3togeo(h3 BIGINT)
 RETURNS VARCHAR
 LAMBDA 'h3-athena-udf-handler',
@@ -25,7 +22,14 @@ LAMBDA 'h3-athena-udf-handler',
 EXTERNAL FUNCTION h3area(h3 BIGINT, unit VARCHAR)
 RETURNS DOUBLE
 LAMBDA 'h3-athena-udf-handler',
+EXTERNAL FUNCTION h3distance(a BIGINT, b BIGINT)
+RETURNS INTEGER
+LAMBDA 'h3-athena-udf-handler',
 
+
+EXTERNAL FUNCTION geotoh3address(lat DOUBLE, lng DOUBLE, res INTEGER)
+RETURNS VARCHAR
+LAMBDA 'h3-athena-udf-handler',
 EXTERNAL FUNCTION h3addresstogeo(h3address VARCHAR)
 RETURNS VARCHAR
 LAMBDA 'h3-athena-udf-handler',
@@ -46,26 +50,13 @@ RETURNS ARRAY<VARCHAR>
 LAMBDA 'h3-athena-udf-handler',
 EXTERNAL FUNCTION h3addressarea(h3address VARCHAR, unit VARCHAR)
 RETURNS DOUBLE
+LAMBDA 'h3-athena-udf-handler',
+EXTERNAL FUNCTION h3addressdistance(a VARCHAR, b VARCHAR)
+RETURNS INTEGER
 LAMBDA 'h3-athena-udf-handler'
-SELECT
-  *,
-  h3isvalid(h3) h3_valid,
-  h3getbasecell(h3) h3_basecell,
-  h3ispentagon(h3) h3_pentagon,
-  h3togeo(h3) h3_point,
-  h3togeoboundary(h3) h3_polygon,
-  h3kring(h3, 3) h3_kring,
-  h3area(h3, 'm2') h3_area,
 
-  h3addressisvalid(h3address) h3address_valid,
-  h3addressgetbasecell(h3address) h3address_basecell,
-  h3addressispentagon(h3address) h3address_pentagon,
-  h3addresstogeo(h3address) h3address_point,
-  h3addresstogeoboundary(h3address) h3address_polygon,
-  h3addresskring(h3address, 3) h3address_kring,
-  h3addressarea(h3address, 'm2') h3address_area
- FROM
-  (
+with tbl1 AS
+(
   SELECT
     lat lat,
     lon lng,
@@ -74,5 +65,33 @@ SELECT
   FROM
     planet
   LIMIT 100
+  ),
+
+tbl2 AS
+  (
+    SELECT
+      h3distance(h3, lag(h3) over ()) h3_distance,
+      h3addressdistance(h3address, lag(h3address) over ()) h3address_distance,
+      *,
+      h3isvalid(h3) h3_valid,
+      h3getbasecell(h3) h3_basecell,
+      h3ispentagon(h3) h3_pentagon,
+      h3togeo(h3) h3_point,
+      h3togeoboundary(h3) h3_polygon,
+      h3kring(h3, 3) h3_kring,
+      h3area(h3, 'm2') h3_area,
+
+      h3addressisvalid(h3address) h3address_valid,
+      h3addressgetbasecell(h3address) h3address_basecell,
+      h3addressispentagon(h3address) h3address_pentagon,
+      h3addresstogeo(h3address) h3address_point,
+      h3addresstogeoboundary(h3address) h3address_polygon,
+      h3addresskring(h3address, 3) h3address_kring,
+      h3addressarea(h3address, 'm2') h3address_area
+    FROM tbl1
   )
+
+SELECT
+  *
+FROM tbl2
 ;
